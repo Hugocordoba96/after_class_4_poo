43eacf2e4e6a2a77eca5eb20addb9d0032beb77b
Daniel Ochoa <danielochoa01@hotmail.com>
2022-10-28 17:07:14 -0600

    clase Clase creada

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from persona import Persona
+#from persona import Persona
 
-persona1 = Persona('Alejo', 'Delgado', 23, '99919240124', 'M')
+#persona1 = Persona('Alejo', 'Delgado', 23, '99919240124', 'M')
 
-print(persona1.get_persona('99919240124'))
+#print(persona1.get_persona('99919240124'))
+
+from clase import Clase
+
+clase1 = Clase('34635', 'Python', 'Programacion', '19-21h', 'lu-mi')
+
+clase1.set_clase()
